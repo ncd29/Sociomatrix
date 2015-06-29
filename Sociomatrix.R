f926f6327058e8fa900d6b31a90d9262f8076b9b
ncd29 <ncd29@cornell.edu>
2015-06-29 12:00:23 -0400

    Fixed attach problem, added more notes

--- Sociomatrix.R
+++ Sociomatrix.R
@@ -4,37 +4,28 @@
 # includes all professors and researchers that professors 
 # at the top 30 Business Schools have collaborated with
 
-data <- read.csv("WOMResearchers.csv", header = T)
-attach(data)
-ns <- paste(First.Name,Last.Name)
-ns2 <- ns[grep("  ", as.character(ns))]
-namesVector <- vector("character",length = 66)
-i = 1
-# remove extra spaces in the middle
-# Why do they exist?
-for (name in ns) {
-  if (name%in%ns2) {
-    newName <- sub(" ","",name)
-    name <- newName
-  }
-  namesVector[i] <- name
-  i = i + 1
-}
-ns <- namesVector
+wom <- read.csv("WOM.csv", header = T, strip.white = T) # remove extra spaces in the middle with strip.white
+ns <- paste(as.character(wom$First.Name),as.character(wom$Last.Name))
 names <- list(ns)
-Collaborators <- as.character(Collaborators)
+Collaborators <- as.character(wom$Collaborators)
 
+# for all researchers, create a vector of all zeros, and change to a 1
+# if they have collaborated with a researcher in the corresponding column
 l <- list()
 for (n in 1:66) {
   if (!is.na(Collaborators[n])) (Collaborators[[n]] <- strsplit(as.character(Collaborators[n]),", "))
-  v <- vector("numeric",length = 66)
+  v <- vector("numeric",length = length(ns))
   for (m in 1:66) {
-    if (ns[m]%in%Collaborators[[n]][[1]]) {
+    if (ns[m]%in%Collaborators[[n]][[1]]) { #if the column researcher is a collaborator of the row researcher
       v[m] = 1
     }
   }
   l[[n]] <- v
 }
+
+# turn the list into a matrix
 sociomatrix <- matrix(unlist(l),nrow = length(ns), ncol = length(ns), dimnames = names)
 colnames(sociomatrix) <- ns
+
 print(sociomatrix)
+
